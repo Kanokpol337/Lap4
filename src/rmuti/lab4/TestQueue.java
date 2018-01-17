b1afbf504e446f9f1c15117e1ce37d3bbcaa3121
Kanokpol337 <dokub337@hotmail.com>
2018-01-17 15:12:22 +0700

    DoDekDee

---
+++ src/rmuti/lab4/TestQueue.java
@@ -0,0 +1,19 @@
+package rmuti.lab4;
+
+public class TestQueue {
+
+
+	public static void main(String[] args) {
+		ArrayListQueue queue = new ArrayListQueue();
+		queue.enqueue("macos");
+		queue.enqueue("Linix");
+		queue.enqueue("windows");
+		System.out.println(queue);
+		
+		String data =(String)queue.dequeue();
+		System.out.println(queue);
+		
+
+	}
+
+}
